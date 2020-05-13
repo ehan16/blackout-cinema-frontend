a2c5a7120bc71d42a291720b774b9238294233e0
Erika Han <e.han@correo.unimet.edu.ve>
2020-05-13 09:03:38 -0400

    cambios en MovieItem

--- src/components/pages/movies/MovieItem.js
+++ src/components/pages/movies/MovieItem.js
@@ -7,12 +7,12 @@ function MovieItem(props) {
                 <h4>{ props.movie.title } <span className="text-capitalize">({ props.movie.language })</span></h4>
                 <ul className="mb-0" style={{ listStyleType: 'none', paddingBottom: '10px' }}>
                     { props.mode === '' ? <li>ID: { props.movie.id }</li> : null }
-                    <li>Año: { props.movie.year }</li>
+                    { props.movie.year !== undefined ? <li>Año: { props.movie.year }</li> : null}
                     <li>Género: { props.movie.genre }</li>
                     <li>Duración: { props.movie.duration } min</li>
                     { props.mode === 'on-air' ? <li>Puestos: { props.movie.lots }</li> : null }
                     { props.mode === 'on-air' ? <li>Sucursal: { props.movie.lots }</li> : null }
-                    { props.mode !== 'to-release' ? <li>Subtítulos: { props.movie.subtitles ? 'Español' : 'No' }</li> : null }
+                    <li>Subtítulos: { props.movie.subtitles ? 'Español' : 'No' }</li>
                     { props.mode === 'to-release' ? <li>Fecha de estreno: { props.movie.date }</li> : null }
                 </ul>
                 <div style={{ display: 'flex' }}>
@@ -20,7 +20,8 @@ function MovieItem(props) {
                     {/* { props.mode === ''  ?  <button style={ deleteStyle }>Eliminar</button> : null }  */}
                     { props.mode === ''  ?  <button style={ editStyle }>Editar</button> : null } 
                     { props.movie.date !== undefined ? <button style={ launchStyle }>Estrenar</button> : null }
-                    { props.movie.lots !== undefined ? <button style={ launchStyle }>Culminar</button> : null }
+                    { props.movie.year === undefined ? <button style={ launchStyle }>Culminar</button> : null }
+                    { props.movie.year === undefined ? <button style={ launchStyle }>Añadir función</button> : null }
                 </div>
             </div>
         </div>
